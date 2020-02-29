a3b9d1c2e61009e9bea71bc723b14f03dc0b020e
shortcipher3 <chris.hall@aggiemail.usu.edu>
2020-02-28 21:33:20 -0700

    Adding configuration info

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -s ${BASEDIR}/bashrc ~/.bashrc
 ln -s ${BASEDIR}/zshrc ~/.zshrc
 ln -s ${BASEDIR}/vimrc ~/.vimrc
 ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+ln -s ${BASEDIR}/gitignore_global ~/.gitignore_global
 ln -s ${BASEDIR}/tmux.conf ~/.tmux.conf
 ln -s ${BASEDIR}/.ssh/authorized_keys ~/.ssh/authorized_keys
 
