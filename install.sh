8dab337e2f86c0e8be4855aa8e3e43f6f50d7dd2
shortcipher3 <chris.jon.hall@gmail.com>
2022-04-15 09:21:25 -0600

    make the directories in case they don't exist

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ ln -s ${BASEDIR}/inputrc ~/.inputrc
 ln -s ${BASEDIR}/gitconfig ~/.gitconfig
 ln -s ${BASEDIR}/gitignore_global ~/.gitignore_global
 ln -s ${BASEDIR}/tmux.conf ~/.tmux.conf
+mkdir -p ~/.ssh
 ln -s ${BASEDIR}/.ssh/authorized_keys ~/.ssh/authorized_keys
+mkdir -p ~/.ipython/profile_default/
 ln -s ${BASEDIR}/ipython_config.py ~/.ipython/profile_default/ipython_config.py
 
